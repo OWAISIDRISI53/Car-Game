fc73c1dc4a8c5b5e53de786146e80bdf9eb8838d
Owais Idrisi <79826730+OWAISIDRISI53@users.noreply.github.com>
2022-04-18 17:13:56 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,14 +4,8 @@ let result = document.querySelector('.result');
 let speed = 100;
 let count = 0;
 
-
 let gameplay = document.querySelector('.gamePlay')
 let gameover = document.querySelector('.gameOver')
-
-
-let overMusic = new Audio('/music')
-let carMusic = new Audio('/sound')
-  carMusic.play()
 
 function left() {
   let carLeft = parseInt(window.getComputedStyle(car).getPropertyValue("left"))
@@ -33,7 +27,6 @@ blueCar.addEventListener('animationiteration', () => {
   blueCar.style.left = r + 'px'
   count++;
   
-  
   if (count > 20) {
     blueCar.style.animationDuration = '0.5s'
   }
@@ -47,19 +40,12 @@ function gameOver() {
   let blueCarTop = parseInt(window.getComputedStyle(blueCar).getPropertyValue("top"))
   let blueCarLeft = parseInt(window.getComputedStyle(blueCar).getPropertyValue("left"));
   carLeft = parseInt(window.getComputedStyle(car).getPropertyValue("left"));
-
   let carDown = parseInt(window.getComputedStyle(car).getPropertyValue("bottom"))
-
   if ((blueCarLeft === carLeft) && (blueCarTop > 290) && (blueCarTop < 380)) {
-    overMusic.play()
-      carMusic.pause()
-
     gameplay.style.display = 'none'
     gameover.style.display = 'block'
     result.innerHTML = `Score: ${count}`;
   }
-
-
 }
 
 setInterval(gameOver, 10)
